43bde5dacbcce0e8ab6cc4d6881f0d6b2f0158a3
alifzulkifeli <alifzulkifeli@gmail.com>
2024-08-22 19:45:54 +0800

    trying pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,36 @@
+import { Metadata } from "next";
 import { Html, Head, Main, NextScript } from "next/document";
 
+
+
+
+
+export const metadata: Metadata = {
+  title: "PWA NextJS",
+  description: "It's a simple progressive web application made with NextJS",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    {
+      name: "imvinojanv",
+      url: "https://www.linkedin.com/in/imvinojanv/",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
+};
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body >
         <Main />
         <NextScript />
       </body>
